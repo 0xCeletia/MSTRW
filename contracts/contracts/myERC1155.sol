14ba9cb32b6922d812da83e4bbca8e64a6f3aa76
Ömer <109610497+jemapelleomer@users.noreply.github.com>
2023-07-13 03:36:22 +0300

    Update myERC1155.sol

--- contracts/contracts/myERC1155.sol
+++ contracts/contracts/myERC1155.sol
@@ -1,1 +1,385 @@
-s
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
+import  "@openzeppelin/contracts/utils/Context.sol";
+import  "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/utils/Arrays.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./modifiedERC1155.sol";
+
+
+contract MyERC1155 is IERC1155MetadataURI, Ownable, modifiedERC1155('uri_/https://game.example/api/item/{id}.json') {
+    
+    using Strings for string;
+    // Mapping from token ID to account balances
+    mapping(uint256 => mapping(address => uint256)) private _balances;
+    // Mapping from account to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+
+    uint256 private _currentTokenID = 0;
+    mapping (uint256 => address) public creators;
+    mapping (uint256 => uint256) public tokenSupply;
+    // contract name
+    string public name;
+    // Contract symbol
+    string public symbol;
+
+    string private baseMetadataURI;
+    string private _uri;
+
+
+
+          /**
+   * @dev Require msg.sender to be the creator of the token id
+   */
+  modifier creatorOnly(uint256 _id) {
+    require(creators[_id] == msg.sender, "ERC1155#creatorOnly: ONLY_CREATOR_ALLOWED");
+    _;
+  }
+
+    /**
+   * @dev Require msg.sender to own more than 0 of the token id
+   */
+  modifier ownersOnly(uint256 _id) {
+    require(_balances[_id][msg.sender] > 0, "ERC1155#ownersOnly: ONLY_OWNERS_ALLOWED");
+    _;
+  }
+
+    constructor(
+    string memory _name,
+    string memory _symbol
+    )
+    {
+    name = _name;
+    symbol = _symbol;
+  }
+
+  /**
+    * @dev returns the basemetadatauri and according to the specific token cnctnts the id
+    */
+  function returnUri(
+    uint256 _id
+  ) public view returns (string memory) {
+    require(_exists(_id), "ERC721Tradable#uri: NONEXISTENT_TOKEN");
+    return string.concat(
+      baseMetadataURI,
+      Strings.toString(_id)
+    );
+  }
+
+    function __setURI(string memory newuri) internal virtual {
+        _uri = newuri;
+    }
+
+
+  /**
+    * @dev Returns the total quantity for a token ID
+    * @param _id uint256 ID of the token to query
+    * @return amount of token in existence
+    */
+  function totalSupply(
+    uint256 _id
+  ) public view returns (uint256) {
+    return tokenSupply[_id];
+  }
+
+  /**
+   * @dev Will update the base URL of token's URI
+   * @param _newBaseMetadataURI New base URL of token's URI
+   */
+  function setBaseMetadataURI(
+    string memory _newBaseMetadataURI
+  ) public onlyOwner {
+    baseMetadataURI = _newBaseMetadataURI;
+  }
+
+  /**
+    * @dev Creates a new token type and assigns _initialSupply to an address
+    * @param _initialOwner address of the first owner of the token
+    * @param _initialSupply amount to supply the first owner
+    * @param __uri Optional URI for this token type // points to a JSON file stored on the internet using IPFS
+    * @param _data Data to pass if receiver is contract
+    * @return The newly created token ID
+    */
+  function create(
+    address _initialOwner,
+    uint256 _initialSupply,
+    string calldata __uri,
+    bytes calldata _data
+  ) external onlyOwner returns (uint256) {
+
+    uint256 _id = _getNextTokenID();
+    _incrementTokenTypeId();
+    creators[_id] = msg.sender;
+
+    if (bytes(__uri).length > 0) {
+      emit URI(__uri, _id);
+    }
+
+    _mint(_initialOwner, _id, _initialSupply, _data);
+    tokenSupply[_id] = _initialSupply;
+    return _id;
+  }
+
+      /**
+    * @dev Mints some amount of tokens to an address
+    * @param _to          Address of the future owner of the token
+    * @param _id          Token ID to mint
+    * @param _quantity    Amount of tokens to mint
+    * @param _data        Data to pass if receiver is contract
+    */
+  function mint(
+    address _to,
+    uint256 _id,
+    uint256 _quantity,
+    bytes memory _data
+  ) public creatorOnly(_id) {
+    _mint(_to, _id, _quantity, _data);
+    tokenSupply[_id] = tokenSupply[_id] + _quantity;
+  }
+
+  /**
+    * @dev Mint tokens for each id in _ids
+    * @param _to          The address to mint tokens to
+    * @param _ids         Array of ids to mint
+    * @param _quantities  Array of amounts of tokens to mint per id
+    * @param _data        Data to pass if receiver is contract
+    */
+  function batchMint(
+    address _to,
+    uint256[] memory _ids,
+    uint256[] memory _quantities,
+    bytes memory _data
+  ) public {
+    for (uint256 i = 0; i < _ids.length; i++) {
+      uint256 _id = _ids[i];
+      require(creators[_id] == msg.sender, "ERC1155Tradable#batchMint: ONLY_CREATOR_ALLOWED");
+      uint256 quantity = _quantities[i];
+      tokenSupply[_id] = tokenSupply[_id] + quantity;
+    }
+    _mintBatch(_to, _ids, _quantities, _data);
+  }
+
+  /**
+    * @dev Change the creator address for given tokens
+    * @param _to   Address of the new creator
+    * @param _ids  Array of Token IDs to change creator
+    */
+  function setCreator(
+    address _to,
+    uint256[] memory _ids
+  ) public {
+    require(_to != address(0), "ERC1155Tradable#setCreator: INVALID_ADDRESS.");
+    require(creators[_ids] == msg.sender, "ERC1155Tradable#batchMint: ONLY_CREATOR_ALLOWED");
+
+    for (uint256 i = 0; i < _ids.length; i++) {
+      uint256 id = _ids[i];
+      _setCreator(_to, id);
+    }
+  }
+
+
+
+    /**
+     * @dev Destroys `amount` tokens of token type `id` from `from`
+     *
+     * Emits a {TransferSingle} event.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `from` must have at least `amount` tokens of token type `id`.
+     */
+    function __burn(address from, uint256 id, uint256 amount) internal {
+        if (from == address(0)) {
+            revert ();
+        }
+        (uint256[] memory ids, uint256[] memory amounts) = __asSingletonArrays(id, amount);
+        _update(from, address(0), ids, amounts, "");
+    }
+
+
+
+    /**
+     * @dev See {IERC1155-balanceOf}.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function _balanceOf(address account, uint256 id) public view virtual returns (uint256) {
+        return _balances[id][account];
+    }
+
+
+    /**
+     * @dev See {IERC1155-balanceOfBatch}.
+     *
+     * Requirements:
+     *
+     * - `accounts` and `ids` must have the same length.
+     */
+    function _balanceOfBatch(
+        address[] memory accounts,
+        uint256[] memory ids
+    ) public view virtual returns (uint256[] memory) {
+        if (accounts.length != ids.length) {
+            // revert (ids.length, accounts.length);
+        }
+
+        uint256[] memory batchBalances = new uint256[](accounts.length);
+
+        for (uint256 i = 0; i < accounts.length; ++i) {
+            batchBalances[i] = balanceOf(accounts[i], ids[i]);
+        }
+
+        return batchBalances;
+    }
+
+    /**
+     * @dev See {IERC1155-setApprovalForAll}.
+     */
+    function _setApprovalForAll(address operator, bool approved) public virtual {
+        _setApprovalForAll(_msgSender(), operator, approved);
+    }
+
+    /**
+     * @dev See {IERC1155-isApprovedForAll}.
+     */
+    function _isApprovedForAll(address account, address operator) public view virtual returns (bool) {
+        return _operatorApprovals[account][operator];
+    }
+
+    /**
+     * @dev See {IERC1155-safeTransferFrom}.
+     */
+    function __safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public virtual {
+        if (from != _msgSender() && !isApprovedForAll(from, _msgSender())) {
+            revert("ERC1155: transfer caller is not owner nor approved");
+        }
+        _safeTransferFrom(from, to, id, amount, data);
+    }
+
+    function __doSafeTransferAcceptanceCheck(
+        address operator,
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) private {
+        if (to.code.length > 0) {
+            try IERC1155Receiver(to).onERC1155Received(operator, from, id, amount, data) returns (bytes4 response) {
+                if (response != IERC1155Receiver.onERC1155Received.selector) {
+                    // Tokens rejected
+                    revert();
+                }
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    // non-ERC1155Receiver implementer
+                    revert();
+                } else {
+                    /// @solidity memory-safe-assembly
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        }
+    }
+
+    function __doSafeBatchTransferAcceptanceCheck(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) private {
+        if (to.code.length > 0) {
+            try IERC1155Receiver(to).onERC1155BatchReceived(operator, from, ids, amounts, data) returns (
+                bytes4 response
+            ) {
+                if (response != IERC1155Receiver.onERC1155BatchReceived.selector) {
+                    // Tokens rejected
+                    revert();
+                }
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    // non-ERC1155Receiver implementer
+                    revert();
+                } else {
+                    /// @solidity memory-safe-assembly
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
+  /**
+    * @dev Change the creator address for given token
+    * @param _to   Address of the new creator
+    * @param _id  Token IDs to change creator of
+    */
+  function _setCreator(address _to, uint256 _id) internal creatorOnly(_id)
+  {
+      creators[_id] = _to;
+  }
+
+  /**
+    * @dev Returns whether the specified token exists by checking to see if it has a creator
+    * @param _id uint256 ID of the token to query the existence of
+    * @return bool whether the token exists
+    */
+  function _exists(
+    uint256 _id
+  ) internal view returns (bool) {
+    return creators[_id] != address(0);
+  }
+
+  /**
+    * @dev calculates the next token ID based on value of _currentTokenID
+    * @return uint256 for the next token ID
+    */
+  function _getNextTokenID() private view returns (uint256) {
+    return _currentTokenID + 1;
+  }
+
+  /**
+    * @dev increments the value of _currentTokenID
+    */
+  function _incrementTokenTypeId() private  {
+    _currentTokenID++;
+  }
+    function __asSingletonArrays(
+        uint256 element1,
+        uint256 element2
+    ) private pure returns (uint256[] memory array1, uint256[] memory array2) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            array1 := mload(0x40)
+            mstore(array1, 1)
+            mstore(add(array1, 0x20), element1)
+
+            array2 := add(array1, 0x40)
+            mstore(array2, 1)
+            mstore(add(array2, 0x20), element2)
+
+            mstore(0x40, add(array2, 0x40))
+        }
+    }
+
+
+}
