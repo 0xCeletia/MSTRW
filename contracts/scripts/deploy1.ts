a56636840233684e13220f8cf5d5e4aa1c6e0b7f
Ömer <109610497+jemapelleomer@users.noreply.github.com>
2023-07-14 23:52:59 +0300

    Update deploy1.ts

--- contracts/scripts/deploy1.ts
+++ contracts/scripts/deploy1.ts
@@ -1,1 +1,21 @@
+import { ethers } from "hardhat";
 
+async function main() {
+  const Mint = await ethers.getContractFactory("Mint");
+  const mint = await Mint.deploy();
+
+  await mint.deployed();
+
+  console.log(`Polygon Mumbai contract deployer to  ${mint.address}`);
+
+  console.log(
+    `Block explorer URL: https://mumbai.polygonscan.com/address/${mint.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
