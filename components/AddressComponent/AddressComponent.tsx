1459ad2dd1d4e945d1b7e1423c56088692ca861a
Ömer <109610497+jemapelleomer@users.noreply.github.com>
2023-07-21 03:51:26 +0300

    Create AddressComponent.tsx

---
+++ components/AddressComponent/AddressComponent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface AddressComponentProps {
+    address: string;
+}
+
+const AddressComponent: React.FC<AddressComponentProps> = ({ address }) => {
+    const shortenAddress = (address: string, digits = 4): string => {
+        if (!address) return "";
+        return `${address.substring(0, digits + 2)}...${address.substring(
+            address.length - digits
+        )}`;
+    };
+
+    return <div>{shortenAddress(address)}</div>;
+};
+
+export default AddressComponent;
