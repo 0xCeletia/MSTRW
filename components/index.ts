a655fb83b3b5b19c1c2189d21f8aa482fc294156
Ömer <109610497+jemapelleomer@users.noreply.github.com>
2023-07-21 03:50:57 +0300

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./Header/Header";
 export { Navbar } from "./Navbar/Navbar";
 export { Container } from "./Container/Container";
+export {default as AddressComponent} from "./AddressComponent/AddressComponent"
