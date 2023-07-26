de77769fa330cf3e7f64ca16b90fe1a9ec500b63
Ömer <109610497+jemapelleomer@users.noreply.github.com>
2023-07-26 20:41:53 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import {
     useWaitForTransaction,
 } from "wagmi";
 import { getAccount } from "@wagmi/core";
-import { Container, Header, Navbar } from "components";
+import { Container, Header, Navbar, Footer } from "components";
 import type { NextPage } from "next";
 import { AiOutlineWallet } from "react-icons/ai";
 import { Button } from "ui";
@@ -45,42 +45,9 @@ import { Progress } from "flowbite-react";
 const Home: NextPage = () => {
     const router = useRouter();
     const [mounted, setMounted] = useState(false);
-    useEffect(() => setMounted(true), []);
-    const { isConnected, isDisconnected } = useAccount();
-    const theme = useTheme();
+    const { isConnected, address } = useAccount();
     const imageRef = useRef<HTMLImageElement | null>(null);
-    const initialPos = useRef<number>(0);
-    const offsetTop = useRef<number>(0);
-    const account = getAccount();
-
-    {
-        /* deniyorum */
-    }
-
-    const address = account.address;
-
-    // const { config: configD } = usePrepareContractWrite({
-    //     address: "0xE22A757FB9F04d90c406D9ede9f5ED75190e4E97",
-    //     abi: contractInterfaceII,
-    //     functionName: "claim",
-    //     args: [
-    //         address,
-    //         "0",
-    //         "1",
-    //         "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-    //         "9000000000000000",
-    //         {
-    //             proof: [
-    //                 "0x0000000000000000000000000000000000000000000000000000000000000000",
-    //             ],
-    //             quantityLimitPerWallet:
-    //                 "115792089237316195423570985008687907853269984665640564039457584007913129639935",
-    //             pricePerToken: "9000000000000000",
-    //             currency: "	0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-    //         },
-    //         "0x",
-    //     ],
-    // });
+    useEffect(() => setMounted(true), []);
 
     const { config: configD } = usePrepareContractWrite({
         address: "0xE22A757FB9F04d90c406D9ede9f5ED75190e4E97",
@@ -117,11 +84,7 @@ const Home: NextPage = () => {
 
     const isClaimed = txSuccessD;
 
-    {
-        /* 
-    supply of all minted
-*/
-    }
+    //    supply of all minted
 
     const [wholeNfts, setWholeNfts] = useState(0);
 
@@ -153,27 +116,7 @@ const Home: NextPage = () => {
             <Navbar />
 
             <section className=" w-full overflow-hidden min-h-screen ">
-                {/* w-[100%] md:w-[85%] bunu silince düzeldi, soldaydı
-                    md:mt-[24px] yukarıyla olan mesafe
-                */}
-                {/* w-16 h-16 yazının değil direkt objenin büyüklüğünü belirliyor
-                    bg-black = background black
-                    py-1 px-8 hareket ettiriyor yazıyı
-                    m-1 objeyi direkt kaydırdı çapraz aşağı
-                    text-sm small demek md medium lg large xl 2xl ******
-                    md:w-32 objeyi büyüttü aynı şey w-16yla gibi
-                    md:text-base kalınlaştırdı ama text-sm'yi etkisiz kıldı
-                    lg:w-48 genişletti ama md:w-48 ve w-48 'den ne farkı var anlamış değilim
-                    lg:text-lg yazıyı genişletti text-sm'yi etkisiz kıldı md:text-base i etkisiz kıldı
-                    mb-[60px] md:mb-[96px] margini sildim yukardan
-                    -------------------------------------------------------------------------------------
-                */}
-                {/* une image pour write onto it
-                    image'ın classında yazan bu direkt genel boyutu belirliyor md:w-[71%]
-                    bg-gradient-to-tr from-purple-200 to-blue-400 sildim
-                    bg-rose-300 sildim
-                    */}
-                <div className="  bg-gradient-to-tr from-rose-300 to-sky-400 bg-cover bg-center h-96 w-[100%] relative">
+                <div className="bg-gradient-to-tr from-rose-300 to-sky-400 bg-cover bg-center h-96 w-[100%] relative">
                     <Image
                         src={Purple}
                         className="hidden h-96 md:flex w-[100%]  object-cover absolute mix-blend-overlay"
@@ -189,46 +132,13 @@ const Home: NextPage = () => {
                         </h2>
                     </div>
                 </div>
-                {/* md:text[] yazının boyutu
-                    GREEN & RED rengini ben tailwind-config den kendim atadım 
-                    md:mt-[24px] idi
-                    */}
-                {/* <h1 className="text-center  text-[32px] md:text-[64px] font-[700] mt-[20px] md:mt-[24px] max-w-[1450px] rubik text-MAIN_DARK dark:text-white mb-0 rubik leading-[40px] md:leading-[72px] ">
-                    INVEST IN MUSIC
-                </h1>
-                <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-MAIN_DARK ">
-                    The Next
-                </h1> */}
-                {/* bu container yeni
-                    classname'de flex-col vardı onu silince h4 yana geldi, aşağıdaydı
-                    classnamedeki md:mt-[96px] üsttekiyle aradaki mesafeyi belirliyor
-                    objeleri ortalıyor
-                    items-center i silince kutu büyüdü
-                */}
                 <Container className="mt-[60px] md:mt-[42px] flex ">
-                    {/* Image className'deki w-[100%] md:w-[75%] kısmının 75 olanı boyutu belirliyor.
-                        -translate-x-20 veya -translate-y-20 y ve x yönünde hareket ettiriyor
-                        */}
                     <Image
                         className=" -translate-x-10 bg-cover bg-center w-[96px] h-[444px] blur-[2px] -translate-y-10 hidden md:flex md:w-[100%] "
                         src={Screens}
                         alt="albumcover!!"
                     />
-                    {/* h4 ve button için yeni bir container açtım şimdi ki alt alta dursunlar
-                        omg oldu ama sağa çekmem gerekiyor yani ortalamalıyım şimdi
-                        wow translate -10 yapabildim butonda ve w-full ile genişlettim
-                        biraz da text i yukarı kaldıralım y-40 yaptım max o kadar
-                        ------------------------------------------------------------
-                        container'a className ekleyerek md:mt-[-50px] ile direkt bu kısmı yani resmin sağ kısmını komple yukarı alabildim
-                        *******************************
-                        margin top aşağı indirdi
-                        space-y-2 içerdeki objeler arası
-                    */}
                     <Container className=" mt-14 -space-y-2 ">
-                        {/* translate-y-10 ile yaklaştırdım alttakine
-                            ******
-                            margin right left ile de hareket ediyor
-                        */}
                         <div className="flex justify-between ">
                             <h4 className=" -translate-y-5 ml-5 md:text-[16px] mt-[20px] font-[500] md:mt-[24px] max-w-[800px] md:leading-[40px] rubik text-emerald-700 dark:text-white">
                                 active listing |
@@ -246,10 +156,6 @@ const Home: NextPage = () => {
                                 | minting phase
                             </h4>
                         </div>
-                        {/* seni yeni ekledim
-                            md:leading-[52px] satır arası boşluk
-                            md:mt-[44px] üstteki itemle olan boşluk
-                        */}
                         <h1 className="  text-[64px] md:text-[24px] md:mt-[-16px] font-[700] text-center text-MAIN_DARK dark:text-white mb-0 rubik leading-[40px] md:leading-[52px] w-[100%] md:w-[85%]">
                             {" "}
                             <span className="text-blue-600">
@@ -266,12 +172,7 @@ const Home: NextPage = () => {
                             /Timeless Tunes
                         </h1>
 
-                        {/* bu div ile arkaya kutu ekledim */}
                         <div className="col bg-neutral-20 dark:bg-green-100 rounded-lg px-4 py-4 md:p-8 shadow-md">
-                            {/* hover:rounded-3xl manyak
-                                ease-linear iyi oluyor
-                                transition-all da olsun
-                            */}
                             {!isConnected && (
                                 <div>
                                     <Button
@@ -284,7 +185,6 @@ const Home: NextPage = () => {
                                 </div>
                             )}
 
-                            {/* sen azcık dur burda */}
                             {mounted && isConnected && !isClaimed && (
                                 <Button
                                     onClick={() => {
@@ -315,30 +215,6 @@ const Home: NextPage = () => {
                                 </Button>
                             )}
 
-                            {/* butonumuzu aazcık değiştirdik */}
-                            {/* {mounted && !isMinted && (
-                                <Button
-                                    onClick={() => mint?.()}
-                                    leftIcon={<AiOutlineWallet />}
-                                    color="openblue"
-                                    className="  w-full -translate-x--8 hover:rounded-3xl hover:bg-blue-300 transition-all cursor-pointer duration-300 ease-linear -translate-y-6 mt-[24px] md:mt-[32px] h-16 pl-4 pr-4 text-[18px]"
-                                    disabled={isMintLoading || isMintStarted}
-                                    data-mint-loading={isMintLoading}
-                                    data-mint-started={isMintStarted}
-                                >
-                                    {isMintLoading && "Waiting for approval"}
-                                    {isMintStarted && "Minting.."}
-                                    {!isMintLoading && !isMintStarted && "Mint"}
-                                </Button>
-                            )} */}
-                            {/* <Button
-                                onClick={() => mint?.()}
-                                leftIcon={<AiOutlineWallet />}
-                                color="openblue"
-                                className="w-full -translate-x--8 hover:rounded-3xl hover:bg-blue-300 transition-all cursor-pointer duration-300 ease-linear -translate-y-6 mt-[24px] md:mt-[32px] h-16 pl-4 pr-4 text-[18px]"
-                            >
-                                mint
-                            </Button> */}
                             {isClaimed && (
                                 <section
                                     className="w-full -translate-x--8
@@ -382,8 +258,6 @@ const Home: NextPage = () => {
                         </div>
                     </Container>
                 </Container>
-                {/* SENDE YENİSİN ALTTAKİ ÖZELLİKLER KISMI OLCAN
-                max-w-[800px] eklersen daralır */}
                 <Container>
                     <div className=" col bg-neutral-100 dark:bg-DARK_PURPLE rounded-lg px-4 py-4 md:p-8 shadow-md">
                         <h1 className="text-[32px] md:text-[44px] font-[700] text-left text-MAIN_DARK dark:text-white mb-0 rubik leading-[40px] md:leading-[72px] w-[100%] md:w-[85%]">
@@ -391,15 +265,6 @@ const Home: NextPage = () => {
                             <span className="text-orange-400">Stats</span> And{" "}
                             <span className="text-green-600"> More</span>{" "}
                         </h1>
-                        {/* with this classname i maanaged to enlist the headings
-                            and with items-center text is centered
-                            --------------------------------------
-                            and if i want objects to side by side
-                            i'd create a container containing the sides like
-                            <Container className="mt-[60px] md:mt-[76px] flex  items-center">
-                            riwallet2line idi
-
-                        */}
                         <Container className="mt-[20px] md:mt-[20px] flex items-center">
                             <Container className=" grid grid-cols-4 ">
                                 <section>
@@ -475,7 +340,6 @@ const Home: NextPage = () => {
                         both the group's exceptional vocal abilites and
                         highlights their growth as producer and songwriters.
                     </h4>
-                    {/* main features ın altındaki özellikler şeyini yapıyor, ortalıyor, yan yana diziyor  */}
                     <div className="grid grid-cols-1 md:grid-cols-2 mt-[32px] space-y-4 md:space-y-0 md:space-x-8">
                         <div className="col bg-neutral-100 dark:bg-DARK_PURPLE rounded-lg px-4 py-4 md:p-8 shadow-md">
                             <div className="bg-slate-400 rounded-full w-[56px] h-[56px] flex justify-center items-center text-MAIN_DARK p-4">
@@ -507,27 +371,7 @@ const Home: NextPage = () => {
                                 </div>
                             </div>
                         </div>
-                        {/* <div className="col bg-neutral-100 dark:bg-DARK_PURPLE rounded-lg px-4 py-4 md:p-8 shadow-md">
-                            <div className="bg-PINK rounded-full w-[56px] h-[56px] flex justify-center items-center text-MAIN_DARK p-4">
-                                <AiFillLock fontSize={48} />
-                            </div>
-                            <h4 className="mt-[12px] text-2xl font-semibold text-DARK_PURPLE dark:text-PINK">
-                                Encryption
-                            </h4>
-                            <h4 className="mt-[8px] text-base font-regular text-neutral-500 dark:text-neutral-300 rubik">
-                                Your notes are encrypted before being stored.
-                                You can make sure that your notes are safe and
-                                only visible to you!
-                            </h4>
-                        </div> */}
-                        {/* note sharing kısmı bu dive ait */}
-                        {/* arkadaki soyut kutu bunun*/}
                         <div className="col bg-neutral-100 dark:bg-DARK_PURPLE rounded-lg px-4 py-4 md:p-8 shadow-md">
-                            {/* alttaki div logoyu ve arkasını hallediyor, içerdeki BsFillShareFill.. logoyu 
-                            RiEarthLine
-                            SlCreditCard
-
-                            */}
                             <div className="bg-slate-400  rounded-full w-[56px] h-[56px] flex justify-center items-center text-MAIN_DARK p-4">
                                 <SlCreditCard fontSize={48} />
                             </div>
@@ -565,35 +409,15 @@ const Home: NextPage = () => {
                         <h2 className="text-center mt-[60px] text-[24px] md:text-[48px] font-[600] rubik text-MAIN_DARK dark:text-white rubik">
                             mint phases
                         </h2>
-                        {/* We are using the latest technologies with high code
-                            quality standard. All the code is open source and we
-                            are open to future contributions! Our primary sales
-                            has not started yet. */}
                         <h4 className="text-center mt-[24px] text-[14px] md:text-[20px] font-[400] rubik text-neutral-500 dark:text-neutral-100 max-w-[512px] ml-auto mr-auto rubik">
                             Better take a seat before it's late!
                         </h4>
 
-                        {/* bu logoları yan yana güzelce dizen de bu */}
                         <div
                             className={`flex flex-wrap mt-[12px] justify-center`}
                         >
-                            {/* <a> */}
-                            {/* <div
-                                    // className=" rounded-full w-[64px] h-[64px] ml-12 flex justify-center items-center text-MAIN_DARK p-4 ">
-                                    className="col flex bg-white dark:bg-DARK_PURPLE p-4 lg:p-8 rounded-xl shadow-lg h-[64px] w-[64px] md:w-[112px] md:h-[112px] hover:-translate-y-2 duration-150 mt-4 md:mt-8 mx-2 md:mx-3"
-                                >
-                                    <TbClockHour12 fontSize={48} color="blue" />
-                                </div>
-                                <p>heyyo</p> */}
-                            {/* <div className="col flex bg-white dark:bg-DARK_PURPLE p-4 lg:p-8 rounded-xl shadow-lg h-[64px] w-[64px] md:w-[112px] md:h-[112px] hover:-translate-y-2 duration-150 mt-4 md:mt-8 mx-2 md:mx-3">
-                                    <h1>phase 1</h1>
-                                </div> */}
-                            {/* </a> */}
                             <a>
-                                <div
-                                    // className=" rounded-full w-[64px] h-[64px] ml-12 flex justify-center items-center text-MAIN_DARK p-4 ">
-                                    className="col flex bg-white dark:bg-DARK_PURPLE p-4 lg:p-8 rounded-xl shadow-lg h-[64px] w-[64px] md:w-[112px] md:h-[112px] hover:-translate-y-2 duration-150 mt-4 md:mt-8 mx-2 md:mx-14"
-                                >
+                                <div className="col flex bg-white dark:bg-DARK_PURPLE p-4 lg:p-8 rounded-xl shadow-lg h-[64px] w-[64px] md:w-[112px] md:h-[112px] hover:-translate-y-2 duration-150 mt-4 md:mt-8 mx-2 md:mx-14">
                                     <TbClockHour3
                                         fontSize={48}
                                         color="#15803d"
@@ -616,10 +440,7 @@ const Home: NextPage = () => {
                                 </div>
                             </a>
                             <a>
-                                <div
-                                    // className=" rounded-full w-[64px] h-[64px] ml-12 flex justify-center items-center text-MAIN_DARK p-4 ">
-                                    className="col flex bg-white dark:bg-DARK_PURPLE p-4 lg:p-8 rounded-xl shadow-lg h-[64px] w-[64px] md:w-[112px] md:h-[112px] hover:-translate-y-2 duration-150 mt-4 md:mt-8 mx-2 md:mx-14"
-                                >
+                                <div className="col flex bg-white dark:bg-DARK_PURPLE p-4 lg:p-8 rounded-xl shadow-lg h-[64px] w-[64px] md:w-[112px] md:h-[112px] hover:-translate-y-2 duration-150 mt-4 md:mt-8 mx-2 md:mx-14">
                                     <TbClockHour6
                                         fontSize={48}
                                         color="#0e7490"
@@ -675,7 +496,7 @@ const Home: NextPage = () => {
                         {/* */}
 
                         <hr className="mt-2"></hr>
-                        <div className="lg:px-[80px]">
+                        <div className="px-[80px]">
                             <Image
                                 ref={imageRef}
                                 src={Flying}
@@ -685,11 +506,6 @@ const Home: NextPage = () => {
                         </div>
                     </Container>
                 </div>
-                {/* h-screen sayfayı uzatıyor haberin ola */}
-                {/* <div className="h-screen"></div> */}
-
-                {/* <div className="h-screen-half hidden md:flex"></div> */}
-                {/* burası ekranı altta kaydıran şey */}
             </section>
 
             <div className="border-t-1 border-neutral-200 dark:border-DARK_PURPLE py-6 mt-[108px]">
