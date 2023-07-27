c7c3859cd7f910d00fe1cf0d5e1788fed0311c0c
Ömer <109610497+jemapelleomer@users.noreply.github.com>
2023-07-27 23:35:15 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,23 @@
-import { useReducer } from "react";
 import "@rainbow-me/rainbowkit/styles.css";
 import merge from "lodash.merge";
 import {
-    darkTheme,
-    getDefaultWallets,
+    connectorsForWallets,
     lightTheme,
     RainbowKitProvider,
     Theme,
 } from "@rainbow-me/rainbowkit";
-
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import type { AppProps } from "next/app";
 import { configureChains, WagmiConfig } from "wagmi";
 import { createClient } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
-import { goerli, optimismGoerli, polygonMumbai } from "@wagmi/core/chains";
+import {
+    goerli,
+    mainnet,
+    polygon,
+    optimismGoerli,
+    polygonMumbai,
+} from "@wagmi/chains";
 import "styles/globals.scss";
 import { RecoilRoot } from "recoil";
 import {
@@ -26,25 +30,66 @@ import {
 import { ThemeOption } from "recoil/theme/types";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { useTheme } from "recoil/theme/ThemeStoreHooks";
-import { scroll } from "consts/chains";
 import { useRouter } from "next/router";
-import { Paths } from "consts/paths";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { Header } from "components";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { rainbowWeb3AuthConnector } from "../RainbowWeb3authConnector";
+import {
+    walletConnectWallet,
+    rainbowWallet,
+    metaMaskWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 
 export const queryClient = new QueryClient();
 
-const { chains, provider, webSocketProvider } = configureChains(
-    [optimismGoerli, polygonMumbai, goerli],
-    [publicProvider()]
-);
+// const { chains, provider, webSocketProvider } = configureChains(
+//     [optimismGoerli, polygonMumbai, goerli],
+//     [
+//         alchemyProvider({ apiKey: "nFTET_K-74PpUAM40EC_9Lx3C8SMn1sW" }),
+//         infuraProvider({ apiKey: "5d95dcb59ef5442c9f75d1ede157a088" }),
+//         publicProvider(),
+//     ]
+// );
 
-const { connectors } = getDefaultWallets({
-    appName: "Masterwave",
-    chains,
-});
+const { chains, provider, webSocketProvider } = configureChains(
+    [mainnet, polygon, goerli],
+    [
+        alchemyProvider({ apiKey: "7wSu45FYTMHUO4HJkHjQwX4HFkb7k9Ui" }),
+        alchemyProvider({ apiKey: "fGXusgBUDC-OPy6XI8IFRvu1i7sbWsYj" }),
+        publicProvider(),
+    ]
+);
+// const { connectors } = getDefaultWallets({
+//     appName: "Masterwave",
+//     chains,
+// });
+
+const connectors = connectorsForWallets([
+    {
+        groupName: "Recommended",
+        wallets: [
+            rainbowWallet({
+                chains,
+                projectId: "0fb24e44681391b35c39748bba4ed484",
+            }),
+            walletConnectWallet({
+                chains,
+                projectId: "0fb24e44681391b35c39748bba4ed484",
+            }),
+            metaMaskWallet({
+                chains,
+                projectId: "0fb24e44681391b35c39748bba4ed484",
+            }),
+            rainbowWeb3AuthConnector({
+                chains,
+                projectId: "0fb24e44681391b35c39748bba4ed484",
+            }),
+        ],
+    },
+]);
+
+// tüm wallet fonksiyonları bu connectorda bitiyor
 
 const wagmiClient = createClient({
     autoConnect: true,
@@ -67,7 +112,6 @@ function masterwaveApp({ Component, pageProps }: AppProps) {
             <ClientOnly>
                 <RecoilRoot>
                     <WagmiConfig client={wagmiClient}>
-                        {/* <ThirdwebProvider activeChain={activeChain}> */}
                         <RainbowKitProvider
                             showRecentTransactions
                             chains={chains}
@@ -78,7 +122,6 @@ function masterwaveApp({ Component, pageProps }: AppProps) {
                             <Component {...pageProps} />
                             <ToastContainer draggable theme={_theme} />
                         </RainbowKitProvider>
-                        {/* </ThirdwebProvider> */}
                     </WagmiConfig>
                 </RecoilRoot>
             </ClientOnly>
